c76fa42722d870ec17950f06bac87ccc6492ecab
DESKTOP-T07ALFP\VoMa <v.malyarov@mail.ru>
2016-09-21 10:25:16 +0200

    new file test.html and test.css dla zrobienia sidebaru

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log('It Works!!!')
 function validateForm() {
   console.log('email', $('#exampleInputEmail1').val());
 }
+
+jQuery(document).ready(function($){
+  $('.icon-menu').click(function(event){
+    $('.sidebar').toggleClass('active')
+  });
+});
