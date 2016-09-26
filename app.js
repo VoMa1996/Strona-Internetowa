ffcd7dafd64edfdfecc5ad021b220723357ff6d8
Volodymyr <v.malyarov2@mail.ru>
2016-09-26 16:07:17 +0200

    new dates in calendar

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ jQuery(document).ready(function($){
     $('.sidebar').toggleClass('active')
   });
 });
+
+
+
+
+function function1() {
+
+  var ul = document.getElementById("days");
+  var all_days = [];
+  var l = 0;
+  for(var k = 1; k <= 30; k++)
+  {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode (k));
+  all_days[l] = li;
+  console.log(all_days[l]);
+  l++;
+  }
+
+
+}
