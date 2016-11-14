0cfbbc6d18b24feb701367666a9bc53167dfdb67
Volodymyr <v.malyarov2@mail.ru>
2016-11-14 16:30:15 +0100

    to do list(mark all for use)

--- Angular/app.js
+++ Angular/app.js
@@ -1,19 +1,70 @@
+var k = 0;
 
 angular
+
   .module('app', [])
   .controller('todoController',['$scope', function($scope){
   $scope.todos = [
-    {'title':'Build a todo app','done':false}
+    {'title':' Build a todo app','done':false}
   ];
+
   $scope.addTodo = function(){
-    $scope.todos.push({'title':$scope.newtodo, 'done':false})
+    k++;
+    $scope.todos.push({'space':" ", 'title':$scope.newtodo, 'done':false})
     $scope.newtodo = ''
+    return $scope.todoss = [
+      {'space':'You have ', 'num':k+1, 'title' : ' list'}
+    ]
+
+
   }
   $scope.clearCompleted = function(){
+      k=-1;
     $scope.todos = $scope.todos.filter(function(item){
+
+      if(!item.done){
+        k++;
+      }
       return !item.done
+
+      $scope.toggleMarkAll = true;
     })
   }
+
+  $scope.clear = function(){
+      k=-1;
+    $scope.todos = $scope.todos.filter(function(item){
+
+
+
+
+
+    })
+  }
+
+  $scope.toggleMarkAll = function() {
+        angular.forEach($scope.todos, function(todo) {
+          todo.done =$scope.markAll;
+
+        });
+    };
+
+
+    // $('#startEngine').attr("src", "Photos/start.mp3");
+    //     $('#startEngine').attr("autostart", "true");
+    //     $('#startEngine').attr("loop", "true");
+    //     $('#startEngine').attr("width", "2");
+    //     $('#startEngine').attr("height", "0");
+
+
+
+  $scope.info = function(){
+    return $scope.todoss = [
+      {'space':'You have ', 'num':k+1, 'title' : ' lists'}
+    ]
+
+  }
+
 }])
 
 
